c1b5be3af397976f478b541bf9a0a5fa6b7749fa
dcthoai <dthoai2k3@gmail.com>
2023-11-30 19:22:14 +0700

    add event click to open form search

--- giava.js
+++ giava.js
@@ -1,7 +1,16 @@
 const showHai = document.getElementById("xuna");
+var formSearch = document.getElementById("hai");
 
 showHai.addEventListener("click", function() {
-    document.getElementById("hai").style.display = "flex";
-
+    if(formSearch.style.display == 'none'){
+        formSearch.style.display = "flex";
+    }else{
+        formSearch.style.display = "none";
+    }
 }); 
 
+window.addEventListener('mousedown', function (event) {
+    if(!formSearch.contains(event.target) && !showHai.contains(event.target)){
+        formSearch.style.display = 'none';
+    }
+})
